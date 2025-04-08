231d1164a5121067ea6440a10195be4a764e1298
mahema1 <51996388+mahema1@users.noreply.github.com>
2025-04-07 20:30:33 -0700

    Time: 8 ms (20.13%), Space: 18 MB (36.17%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,11 +5,31 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # dummy= ListNode()
+        # curr= dummy
+        # carry=0
+        # while l1 or l2 or carry:
+        #     v1= l1.val if l1 else 0 
+        #     v2= l2.val if l2 else 0
+        #     val= v1+v2+carry
+
+        #     carry= val//10
+        #     val= val%10
+        #     curr.next= ListNode(val)
+
+        #     curr=curr.next 
+        #     l1= l1.next if l1 else None
+        #     l2= l2.next if l2 else None
+
+        # return dummy.next
+
+
         dummy= ListNode()
         curr= dummy
         carry=0
-        while l1 or l2 or carry:
-            v1= l1.val if l1 else 0 
+
+        while l1 or l2:
+            v1= l1.val if l1 else 0
             v2= l2.val if l2 else 0
             val= v1+v2+carry
 
@@ -17,11 +37,14 @@ class Solution:
             val= val%10
             curr.next= ListNode(val)
 
-            curr=curr.next 
+            curr= curr.next
             l1= l1.next if l1 else None
             l2= l2.next if l2 else None
 
+        if carry>0:
+            curr.next= ListNode(carry)
         return dummy.next
+
 
 
 
