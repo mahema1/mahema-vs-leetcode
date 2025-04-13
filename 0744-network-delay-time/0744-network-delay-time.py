609921e64ab01b693550ddaaf8551ee2d79fbada
Mahema Reddy Nelaturi <51996388+mahema1@users.noreply.github.com>
2025-04-13 15:15:13 -0700

    Time: 393 ms (38.36%), Space: 19.8 MB (56.36%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -4,18 +4,20 @@ class Solution:
         for u,v, time in times:
             g[u].append((v,time))
         mintime={}
-        min_heap=[(0,k)] # distance from source to node, node
+        minheap=[(0,k)] # time, node
 
-        while (min_heap):
-            time_k_to_i, i= heapq.heappop(min_heap)
+        while(minheap):
+            time, i= heapq.heappop(minheap)
             if i in mintime:
                 continue
             
-            mintime[i]= time_k_to_i
+            mintime[i]= time
             for nei, nei_time in g[i]:
-                heapq.heappush(min_heap, (time_k_to_i + nei_time, nei))
+                if nei not in mintime:
+                    heapq.heappush(minheap, (time+nei_time, nei))
         if len(mintime)==n:
             return max(mintime.values())
         else:
             return -1
+
         
