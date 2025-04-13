624e810c755a856ce4135a3c53d3e5fe6862589f
Mahema Reddy Nelaturi <51996388+mahema1@users.noreply.github.com>
2025-04-13 10:12:17 -0700

    Time: 9 ms (19.94%), Space: 19.8 MB (12.05%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,32 +1,31 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        # time: (V+E), space: O(V)
         pre= prerequisites
         g= defaultdict(list)
         for a,b in pre:
             g[a].append(b)
-        
-        unvisited= 0
+
         visiting=set()
         visited=set()
-        # states=[unvisited] * numCourses 
 
         def dfs(node):
-            # state= states[node]
-            if node in visited: return True
-
-            if node in visiting: return False
-
+            if node in visited:
+                return True
+            if node in visiting:
+                return False
             visiting.add(node)
 
             for nei in g[node]:
                 if not dfs(nei):
                     return False
+            visiting.remove(node)
             visited.add(node)
             return True
-        
         for i in range(numCourses):
             if not dfs(i):
                 return False
-        
         return True
+
         
