6a5bf22775aeaf3f561d94a292d72ef649e98607
Mahema Reddy Nelaturi <51996388+mahema1@users.noreply.github.com>
2025-04-13 17:04:00 -0700

    Time: 25 ms (50.61%), Space: 19.4 MB (23.73%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -3,20 +3,17 @@ class Solution:
         if not grid:
             return 0
         rows, cols= len(grid), len(grid[0])
+        visit=set()
 
         def dfs(r,c):
-            if (r<0 or r>=rows or c<0 or c>=cols or grid[r][c] ==0 or (r,c) in visit):
+            if (r<0 or c<0 or r>= rows or c>= cols or grid[r][c]!=1 or (r,c) in visit):
                 return 0
             visit.add((r,c))
-            return (1+ dfs(r+1, c)+ dfs(r-1,c)+dfs(r, c+1)+ dfs(r, c-1))
-
-
-        visit= set()
-        area=0
+            return 1+ dfs(r+1, c)+dfs(r-1, c)+ dfs(r,c+1)+ dfs(r, c-1)
+        maxarea=0
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c]==1 and (r,c) not in visit:
-                    area= max(area, dfs(r,c))
-        return area
-
-        
+                if grid[r][c]== 1 and (r,c) not in visit:
+                    area= dfs(r,c)
+                    maxarea= max(area, maxarea)        
+        return maxarea
