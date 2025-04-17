d7b64d5173d4105f7892a2d298df108332ad4c44
Mahema Reddy Nelaturi <51996388+mahema1@users.noreply.github.com>
2025-04-17 15:26:24 -0700

    Time: 37 ms (65.23%), Space: 17.7 MB (48.98%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -6,36 +6,11 @@ class Solution:
             return word2
         if not word2:
             return word1
-        
-        while i< len(word1) and j<len(word2):
+        while i<len(word1) and j<len(word2):
             res.append(word1[i])
             res.append(word2[j])
             i+=1
             j+=1
         res.append(word1[i:])
         res.append(word2[j:])
-
-        return ("".join(res))
-        # res=[]
-        # if not word1:
-        #     return word2
-        # if not word2:
-        #     return word1
-        
-        # for i in range(min(len(word1), len(word2))):
-        #     res.append(word1[i])
-        #     res.append(word2[i])
-        
-        # if len(word1)> len(word2):
-        #     for ch in word1[len(word2):]:
-        #         res.append(ch)
-        # if len(word2)> len(word1):
-        #     for ch in word2[len(word1):]:
-        #         res.append(ch)
-        
-
-        # return ("".join(res))
-
-
-
-        
+        return "".join(res)
