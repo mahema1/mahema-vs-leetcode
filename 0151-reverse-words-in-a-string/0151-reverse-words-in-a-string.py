a3b1020badd81d6ebeb470642a31a5febac3414c
Mahema Reddy Nelaturi <51996388+mahema1@users.noreply.github.com>
2025-04-17 22:17:48 -0700

    Time: 3 ms (32.74%), Space: 18.1 MB (5.28%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,12 +1,25 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
+        # # we can use a stack -O(n)space, else two pointers.
+        # words= s.split()
+        # l,r=0, len(words)-1
+        # while l<r:
+        #     words[l], words[r]= words[r], words[l]
+        #     l+=1
+        #     r-=1
+        # return " ".join(words)
+
+        ## time: O(n), space: O(1)
+
         words= s.split()
-        l,r=0, len(words)-1
-        while l<r:
-            words[l], words[r]= words[r], words[l]
-            l+=1
-            r-=1
-        return " ".join(words)
+        stack=[]
+        res=[]
+        for word in words:
+            stack.append(word)
+            
+        while stack:
+            res.append(stack.pop())
+        return " ".join(res)
         
         
         
