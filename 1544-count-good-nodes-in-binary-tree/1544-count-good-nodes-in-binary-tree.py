6cb0e539e0fa53debcc6610216d0801b4e7fe373
Mahema Reddy Nelaturi <51996388+mahema1@users.noreply.github.com>
2025-04-19 16:05:05 -0700

    Time: 141 ms (62.83%), Space: 32.2 MB (33.26%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -7,34 +7,35 @@
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
 
-        # #DFS time: O(n), space: O(n)
-        
-        # def dfs(node, maxVal):
-        #     if not node:
-        #         return 0
-        #     res=1 if node.val >= maxVal else 0
-        #     maxVal= max(maxVal, node.val)
-        #     res+=dfs(node.left,maxVal) 
-        #     res+=dfs(node.right, maxVal)
-        #     return res
-        # return dfs(root, root.val)
-        
-
-        #BFS
-        res=0
-        q= deque()
-        if not root:
-            return 0
-        q.append((root, root.val))
-        while q:
-            node, maxVal= q.popleft()
-
+        #DFS time: O(n), space: O(n)
+        self.good=0
+        def dfs(node, maxVal):
             if node.val >= maxVal:
-                res+=1
-            maxVal= max(maxVal, node.val)
+                self.good+=1
             if node.left:
-                q.append((node.left, maxVal))
+                dfs(node.left, max(maxVal, node.val))
             if node.right:
-                q.append((node.right, maxVal))
-        return res
+                dfs(node.right, max(maxVal, node.val))
+        dfs(root, root.val)
+        return self.good
+            
+        
+
+        # #BFS
+        # res=0
+        # q= deque()
+        # if not root:
+        #     return 0
+        # q.append((root, root.val))
+        # while q:
+        #     node, maxVal= q.popleft()
+
+        #     if node.val >= maxVal:
+        #         res+=1
+        #     maxVal= max(maxVal, node.val)
+        #     if node.left:
+        #         q.append((node.left, maxVal))
+        #     if node.right:
+        #         q.append((node.right, maxVal))
+        # return res
 
