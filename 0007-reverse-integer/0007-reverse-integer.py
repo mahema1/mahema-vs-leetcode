4bd529959fa6eddea999be4c0cf72c69288c1533
Mahema Reddy Nelaturi <51996388+mahema1@users.noreply.github.com>
2025-04-18 20:22:42 -0700

    Time: 46 ms (14.93%), Space: 17.8 MB (57.19%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        y= abs(x)
+
+        new=0
+        while y!=0:
+            last= y%10
+            new= new*10+ last
+            y=y//10
+
+        new = new if x > 0 else -new
+
+        # Check 32-bit signed integer range
+        if new < (-2**31) or new > (2**31 - 1):
+            return 0
+
+        return new
+
+
