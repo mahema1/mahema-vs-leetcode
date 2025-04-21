89aa646a3c3d611b6047723a55b8f3ace3f912b9
Mahema Reddy Nelaturi <51996388+mahema1@users.noreply.github.com>
2025-04-21 12:07:47 -0700

    Time: 5 ms (38.78%), Space: 19.8 MB (14.17%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hashmap=defaultdict(int)
+        for num in nums:
+            hashmap[num]+=1
+
+        for key, value in hashmap.items():
+            if value==1:
+                return key
+        
