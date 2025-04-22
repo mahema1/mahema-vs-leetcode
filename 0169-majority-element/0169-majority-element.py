e75a86d3ff3a037462b21760ae442c9d72b1b20b
Mahema Reddy Nelaturi <51996388+mahema1@users.noreply.github.com>
2025-04-21 19:49:16 -0700

    Time: 12 ms (21.53%), Space: 19.2 MB (97.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,13 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # hashmap=defaultdict(int)
-        # for i in range(len(nums)):
-        #     hashmap[nums[i]]+=1
-        # n= len(nums)//2
+        hashmap=defaultdict(int)
+        for i in range(len(nums)):
+            hashmap[nums[i]]+=1
+        n= len(nums)//2
         # for num in hashmap:
         #     if hashmap[num] > n:
         #         return num
+        for key, val in hashmap.items():
+            if val>n:
+                return key
 
-        nums.sort()
-        return nums[len(nums)//2]
+        # nums.sort()
+        # return nums[len(nums)//2]
         
