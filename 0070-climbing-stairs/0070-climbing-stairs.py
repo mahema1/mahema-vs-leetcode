ee24e4a7a4d58b7817181deeacb788076f4674be
Mahema Reddy Nelaturi <51996388+mahema1@users.noreply.github.com>
2025-04-30 17:13:15 -0700

    Time: 0 ms (100%), Space: 17.7 MB (78.33%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,26 +1,26 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
 
-        # #time: O(n), space:O(n)
-        # if n==1:
-        #     return 1
-        # if n==2:
-        #     return 2
-        # dp=[0]*n
-        # dp[0]=1
-        # dp[1]=2
-        # for i in range(2, n):
-        #     dp[i]= dp[i-2]+dp[i-1]
-
-        # return dp[n-1]
-
-        #time: O(n), space: O(1)
+        #time: O(n), space:O(n)
         if n==1:
             return 1
         if n==2:
             return 2
-        prev=1
-        cur=2
+        dp=[0]*n
+        dp[0]=1
+        dp[1]=2
         for i in range(2, n):
-            prev, cur= cur, prev+cur
-        return cur
+            dp[i]= dp[i-2]+dp[i-1]
+
+        return dp[n-1]
+
+        # #time: O(n), space: O(1)
+        # if n==1:
+        #     return 1
+        # if n==2:
+        #     return 2
+        # prev=1
+        # cur=2
+        # for i in range(2, n):
+        #     prev, cur= cur, prev+cur
+        # return cur
